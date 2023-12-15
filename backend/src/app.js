0ebb8c4a2126d6bd7d679378d34c9e02526a9a71
Marcelo Diaz <marceloardiaz@gmail.com>
2023-12-15 12:49:49 -0300

    feat(backend): feature payment (#84)

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,12 +9,13 @@ const userRouter = require('./core/users/users.router');
 const authRouter = require('./core/auth/auth.router');
 const maintenanceRouter = require('./core/maintenance/maintenance.router');
 const condominuimsRouter = require('./core/condominiums/condominiums.router');
+const paymentsRouter = require('./core/payments/payments.router');
 const complaintRouter = require('./core/complaint/complaint.router');
 const amenitieRouter = require('./core/amenities/amenities.router');
 const initModels = require('./models/initModels');
 const path = require('path');
 
-app.use(logger('dev'))
+app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
@@ -28,14 +29,12 @@ db.authenticate()
   });
 
 db.sync()
-    .then(()=> {
-        console.log('Database Synced')
-    })
-    .catch(err => {
-        console.log(err)
-    })
-
-
+  .then(() => {
+    console.log('Database Synced');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get('/api/v1', (req, res) => {
   res.status(200).json({
@@ -46,6 +45,7 @@ app.get('/api/v1', (req, res) => {
     maintenance: `localhost:${port}/api/v1/maintenance`,
     complaint: `localhost:${port}/api/v1/complaint`,
     condominuims: `localhost:${port}/api/v1/condominuims`,
+    payments: `localhost:${port}/api/v1/payments`
   });
 });
 
@@ -54,11 +54,12 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/amenities', amenitieRouter);
 app.use('/api/v1/maintenance', maintenanceRouter);
 app.use('/api/v1/complaint', complaintRouter);
-app.use("/api/v1/condominuims", condominuimsRouter )
+app.use('/api/v1/condominuims', condominuimsRouter);
+app.use('/api/v1/payments', paymentsRouter);
 
-app.get('*', (req,res) =>{
-   res.sendFile(path.join(__dirname,'../public/index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
