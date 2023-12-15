5e2bbce5441743a6f607775f13bc8e09dc35ba81
Alberto Gomez Juan <alberto.gomez.j@bairesdev.com>
2023-12-15 23:30:00 +0100

    fix(backend): :bug: added host 0.0.0.0

--- backend/src/app.js
+++ backend/src/app.js
@@ -61,6 +61,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server started on port ${port}`);
 });
