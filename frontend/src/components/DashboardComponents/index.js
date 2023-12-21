d47ce0aa23b5bf688c68da2a124e21a2e82cdda8
Fabian Romano <fabian.ed.romano@gmail.com>
2023-12-21 16:52:35 -0300

    feat(frontend): tablas del home Amenidades-Pagos-Mantenimientos

--- frontend/src/components/DashboardComponents/index.js
+++ frontend/src/components/DashboardComponents/index.js
@@ -1,3 +1,5 @@
 export { default as PaymentsChart } from './PaymentsChart';
 export { default as TableMaintenance } from './TableMaintenance';
 export { default as TableTenants } from './TableTenants';
+export { default as TableAmenities } from './TableAmenities';
+export { default as TablePayments } from './TablePayments';
